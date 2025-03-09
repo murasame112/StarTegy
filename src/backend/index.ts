8553ebe958a9e0e22fe73ed094ceb85cfe848489
murasame112 <tomaszwiesek00@gmail.com>
2025-03-10 00:08:04 +0100

    endpoints in index

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,19 +2,23 @@ import { Console } from 'console';
 import express from 'express';
 import { Request, Response } from 'express';
 import * as mongoClient from "./mongodb/connection"
-
+import * as strategyEndpoints from "./endpoints/strategy_endpoints";
 
 const app = express();
 app.use(express.json());
 mongoClient.run();
 
-app.get('/test', testAPI);
 
-function testAPI(req: Request, res: Response){
-	const result = mongoClient.getAllItems('strategies');
-	result.then((value) => {
-			res.status(200).send(value);
-	});
-}
+
+app.get('/all', strategyEndpoints.getAllStrategies);
+
+app.get('/strategy/:id', strategyEndpoints.getStrategyById);
+
+// test endpoint
+app.post("/teststrat", strategyEndpoints.postTestStrategy);
+
+app.post("/strategy", strategyEndpoints.insertStrategy);
+
+app.delete("/strategy/:id", strategyEndpoints.deleteStrategy);
 
 app.listen(4200);
