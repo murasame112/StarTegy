79d289e3bdc9093c1c6cf748d2d667a2473f9ead
murasame112 <tomaszwiesek00@gmail.com>
2025-03-10 19:01:54 +0100

    footer

---
+++ src/frontend/startegy-frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <div className={styles.footer}>
+            <div className={styles.content}>
+                <p>Web Design by Tomasz Więsek, 2024</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
