4a7830e50001bd1c81f5e5cbfc0ac27f400392b0
murasame112 <tomaszwiesek00@gmail.com>
2025-03-11 17:13:13 +0100

    strat list item

---
+++ src/frontend/startegy-frontend/src/components/StratListItem/StratListItem.tsx
@@ -0,0 +1,64 @@
+import { Strategy } from '../../models/strategy_model';
+import { ListedStrategy } from '../../models/listed_strategy_model';
+
+
+// ===== styles =====
+import stylesDefault from '../../index.css';
+import styles from './StratListItem.module.css';
+
+// ===== pictures =====
+import zerg_pic from '/pictures/races/zerg_icon.png';
+import terran_pic from '/pictures/races/terran_icon.png';
+import protoss_pic from '/pictures/races/protoss_icon.png';
+import star from '/pictures/misc/star-48.png';
+
+
+function getPic(race: string) {
+    switch (race) {
+        case 'Zerg':
+            return zerg_pic;
+        case 'Terran':
+            return terran_pic;
+        case 'Protoss':
+            return protoss_pic;
+    }
+}
+
+function StratListItem(props: ListedStrategy) {
+    const matchups = props.matchup.join(', ');
+
+    return (
+        <div className={styles.box}>
+            <div className={styles.starBox}>
+                <img src={star} alt='star' className={styles.star} />
+            </div>
+            <div className={styles.pictureBox}>
+                <img
+                    src={getPic(props.race)}
+                    alt='race_picture'
+                    className={styles.picture}
+                />
+            </div>
+            <div className={styles.titleBox}>
+                <p className={styles.title}>{props.title}</p>
+            </div>
+            <div className={styles.typeBox}>
+                <p>{props.type}</p>
+            </div>
+            <div className={styles.dateBox}>
+                {/* <p>added {props.date?.toLocaleDateString("en-GB")}</p> TODO: */}
+            </div>
+            <div className={styles.matchupBox}>
+                <p>{matchups}</p>
+            </div>
+            <div className={styles.buildTypeBox}>
+                <p>{props.build_type}</p>
+            </div>
+            <button className={styles.tagsButton}>Tags</button>
+        </div>
+    );
+}
+
+//StratListItem.propTypes = Strategy.propTypes
+
+export default StratListItem;
