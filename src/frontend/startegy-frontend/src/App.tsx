839e2c60fc182043ff458c82c70af34af8904b2b
murasame112 <tomaszwiesek00@gmail.com>
2025-03-11 17:36:16 +0100

    app

--- src/frontend/startegy-frontend/src/App.tsx
+++ src/frontend/startegy-frontend/src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// ===== components =====
+import Blank from './components/Blank/Blank';
+import StratList from './components/StratList/StratList';
+import StratSingle from './components/StratSingle/StratSingle';
+import StratNew from './components/StratNew/StratNew';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import TempNavbar from './components/TempNavbar/TempNavbar';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Navigate,
+		useLocation
+} from 'react-router-dom';
+
+// ===== styles =====
+import './index.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+			<Router>
+			<Header/>
+        <div className='content'>
+					<TempNavbar/>
+                {/* <Link to="/strategy/id" >display strategy</Link>  */}
+                {/* <Link to="/edit/id" >edit strategy</Link>  */}
+                {/* <Link to="/players" >Players list</Link> */}
+                {/* <Link to="/profile/id" >user profile</Link>  */}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <Routes>
+                    <Route path='/strategies' element={<StratList />}></Route>
+                    <Route
+                        path='/strategy/:id'
+                        element={<StratSingle />}
+                    ></Route>
+                    <Route path='/create' element={<StratNew/>}></Route>
+                    <Route path='/edit/id'></Route>
+                    <Route path='/players'></Route>
+                    <Route path='/profile/id'></Route>
+                    <Route
+                        path='*'
+                        element={<Navigate to='/' replace />}
+                    ></Route>
+                </Routes>
+        </div>
+				<Footer/>
+				</Router>
+    );
 }
 
-export default App
+export default App;
