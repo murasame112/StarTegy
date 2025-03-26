16d319e19ace21ef81178a27e802199d705b03ae
murasame112 <tomaszwiesek00@gmail.com>
2025-03-26 14:54:50 +0100

    filter checkboxes work

--- src/frontend/startegy-frontend/src/components/StratList/StratList.tsx
+++ src/frontend/startegy-frontend/src/components/StratList/StratList.tsx
@@ -15,6 +15,21 @@ function StratList() {
     const [data, setData] = useState<Strategy[]>([]);
 		const [filteredData, setFilteredData] = useState<Strategy[]>([]);
 		const [filtersVisible, setFiltersVisible] = useState<boolean>(false);
+		const [filters, setFilters] = useState<{ [key: string]: boolean }>({
+			"Protoss": false,
+			"PvP": false,
+			"PvT": false,
+			"PvZ": false,
+			"Terran": false,
+			"TvP": false,
+			"TvT": false,
+			"TvZ": false,
+			"Zerg": false,
+			"ZvP": false,
+			"ZvT": false,
+			"ZvZ": false,
+
+		});
 
 
     useEffect(() => {
@@ -45,42 +60,77 @@ function StratList() {
 			setFilteredData(filtered);
 		};
 
-		const filtersButton = () => {
+		const filtersChange = (event: any) => {
+			const { name, checked } = event.target;
+			console.log(name);
 
+			setFilters((prev) => {
+				const updatedFilters: any = {
+					...prev,
+					[name]: checked,
+				};
+		
+				const selectedFilters = Object.keys(updatedFilters).filter((key) => updatedFilters[key]);
+				
+				return updatedFilters;
+			});
 		}
 
     return (
         <>
-					<div className='card' ref={divRef} style={{ minHeight }}>
+            <div className='card' ref={divRef} style={{ minHeight }}>
                 <div className={styles.upperBar}>
-									<input
+                    <input
                         type='text'
                         placeholder='Search'
                         className={styles.textInput}
                         onChange={search}
-                  />
-									<div className={styles.buttonsDiv}>
-										<Link to={'/create'}><button className={styles.addNewButton + ' buttonPrimary'}>Add new strategy</button></Link>
-										<button className={filtersVisible ? 'buttonSecondaryActive' : 'buttonSecondary'} onClick={() => { setFiltersVisible(!filtersVisible)}}>Filter</button>
-									</div>
+                    />
+                    <div className={styles.buttonsDiv}>
+                        <Link to={'/create'}>
+                            <button
+                                className={
+                                    styles.addNewButton + ' buttonPrimary'
+                                }
+                            >
+                                Add new strategy
+                            </button>
+                        </Link>
+                        <button
+                            className={
+                                filtersVisible
+                                    ? 'buttonSecondaryActive'
+                                    : 'buttonSecondary'
+                            }
+                            onClick={() => {
+                                setFiltersVisible(!filtersVisible);
+                            }}
+                        >
+                            Filter
+                        </button>
+                    </div>
 
-									{filtersVisible && <div className={styles.filters}>
-										<label htmlFor='protoss_input' className={styles.filterInputHigh}><input id='protoss_input' className={styles.checkbox} type='checkbox'/>Protoss</label>
-										<label htmlFor='pvp_input' className={styles.filterInputLow}><input id='pvp_input' className={styles.checkbox} type='checkbox'/>PvP</label>
-										<label htmlFor='pvt_input' className={styles.filterInputLow}><input id='pvt_input' className={styles.checkbox} type='checkbox'/>PvT</label>
-										<label htmlFor='pvz_input' className={styles.filterInputLow}><input id='pvz_input' className={styles.checkbox} type='checkbox'/>PvZ</label>
-
-										<label htmlFor='terran_input' className={styles.filterInputHigh}><input id='terran_input' className={styles.checkbox} type='checkbox'/>Terran</label>
-										<label htmlFor='tvp_input' className={styles.filterInputLow}><input id='tvp_input' className={styles.checkbox} type='checkbox'/>TvP</label>
-										<label htmlFor='tvt_input' className={styles.filterInputLow}><input id='TvT_input' className={styles.checkbox} type='checkbox'/>TvT</label>
-										<label htmlFor='tvz_input' className={styles.filterInputLow}><input id='TvZ_input' className={styles.checkbox} type='checkbox'/>TvZ</label>
-
-										<label htmlFor='zerg_input' className={styles.filterInputHigh}><input id='zerg_input' className={styles.checkbox} type='checkbox'/>Zerg</label>
-										<label htmlFor='zvp_input' className={styles.filterInputLow}><input id='zvp_input' className={styles.checkbox} type='checkbox'/>ZvP</label>
-										<label htmlFor='zvt_input' className={styles.filterInputLow}><input id='zvt_input' className={styles.checkbox} type='checkbox'/>ZvT</label>
-										<label htmlFor='zvz_input' className={styles.filterInputLow}><input id='zvz_input' className={styles.checkbox} type='checkbox'/>ZvZ</label>
-									</div>}
-
+                    {filtersVisible && (
+                        <div className={styles.filters}>
+                            {Object.keys(filters).map((key) => (
+                                <label
+                                    key={key}
+                                    htmlFor={key + '_input'}
+                                    className={key === 'Protoss' || key === 'Terran' || key === 'Zerg' ? styles.filterInputHigh : styles.filterInputLow}
+                                >
+                                    <input
+                                        name={key}
+                                        id={key + '_input'}
+																				className={styles.checkbox}
+                                        checked={filters[key]}
+                                        onChange={filtersChange}
+                                        type='checkbox'
+                                    />
+                                    {key}
+                                </label>
+                            ))}
+                        </div>
+                    )}
                 </div>
                 <div className={styles.listBox}>
                     {filteredData.map((item, i) => (
@@ -91,7 +141,7 @@ function StratList() {
                                 matchup={item.matchup}
                                 date={item.date}
                                 author={item.author}
-																uploaded_by={item.uploaded_by}
+                                uploaded_by={item.uploaded_by}
                                 type={item.type}
                                 tags={item.tags}
                                 build_type={item.build_type}
@@ -99,7 +149,7 @@ function StratList() {
                         </Link>
                     ))}
                 </div>
-						</div>
+            </div>
         </>
     );
 }
