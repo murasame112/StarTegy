86adf9e3985970743b8f9402072bed4954117294
murasame112 <tomaszwiesek00@gmail.com>
2025-04-22 18:55:51 +0200

    check if user exists

--- src/backend/services/login_service.ts
+++ src/backend/services/login_service.ts
@@ -3,6 +3,15 @@ import passwordHash from 'password-hash';
 import { User } from "../models/user_model";
 import fs from 'fs';
 import path from 'path';
+import * as mongoClient from '../mongodb/connection';
+
+export async function checkIfUserExists(userEmail: string, userLogin: string){
+	const result = await mongoClient.getItemsByField({"email": userEmail, "login": userLogin}, 'users');
+	if(result.length > 0){
+		return true;
+	}
+	return false;
+}
 
 export async function login(login: string, password: string) {}
 // TODO: login service
