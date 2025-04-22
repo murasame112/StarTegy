ca2f54369de4559193c97f353a9510ec6f0c8330
murasame112 <tomaszwiesek00@gmail.com>
2025-04-22 18:48:12 +0200

    login imports

--- src/backend/services/login_service.ts
+++ src/backend/services/login_service.ts
@@ -1,2 +1,8 @@
+import jwt from 'jsonwebtoken';
+import passwordHash from 'password-hash';
+import { User } from "../models/user_model";
+import fs from 'fs';
+import path from 'path';
+
 export async function login(login: string, password: string) {}
 // TODO: login service
