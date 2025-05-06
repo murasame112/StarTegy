0a9dc846892a326f3efcf57e22ad6607704a5960
murasame112 <tomaszwiesek00@gmail.com>
2025-05-06 19:34:41 +0200

    user model

---
+++ src/frontend/startegy-frontend/src/models/user_model.ts
@@ -0,0 +1,29 @@
+import { ObjectId } from "bson";
+
+export class User {
+  login: string;
+  email: string;
+  password: string;
+  active: boolean; // false means banned
+  strategies: Array<ObjectId>;
+	created: Date;
+	_id?: ObjectId;
+
+  constructor(
+    login: string,
+    email: string,
+    password: string,
+    active?: boolean,
+    strategies?: Array<ObjectId>,
+		created?: Date,
+		_id?: ObjectId
+  ) {
+    this.login = login;
+    this.email = email;
+    this.password = password;
+    this.active = active ? active : true;
+    this.strategies = strategies ? strategies : [];
+		this.created = created ? created : new Date();
+		this._id = _id ? _id : undefined;
+  }
+}
